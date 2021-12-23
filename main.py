e7a28a11097da383b39a84b2e699902f7c3ec364
U-DESKTOP-BR71KST\Damien <damien.brown15@outlook.com>
2021-12-23 09:18:51 -0800

    Add usage of .env and logging

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import discord
+import os
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+token = os.environ.get("token")
 client = discord.Client()
 @client.event
 async def on_ready():
@@ -10,4 +19,4 @@ async def on_message(message):
         return
     if message.content == "Hello":
         await message.channel.send('World!')
-client.run(TOKEN)
+client.run(token)
