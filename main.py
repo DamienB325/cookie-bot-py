c071fcf262d778a1a31ffbfec8417bbed8f8942b
U-DESKTOP-BR71KST\Damien <damien.brown15@outlook.com>
2021-12-23 15:26:41 -0800

    Add commands! :)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import logging
+from discord.ext import commands
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -8,15 +9,14 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 token = os.environ.get("token")
-client = discord.Client()
-@client.event
+bot = commands.Bot(command_prefix="$")
+@bot.event
 async def on_ready():
     print("The bot is ready!")
-    
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == "Hello":
-        await message.channel.send('World!')
-client.run(token)
+
+
+@bot.command()
+async def ping(ctx):
+	await ctx.channel.send("pong")
+
+bot.run(token)
